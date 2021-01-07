a1690df7c04a537138bd57d42c872afbe2df7675
Luisa <luisamue@hotmail.de>
2021-01-07 10:33:14 +0100

    pom mockito

---
+++ ErstesMavenProject/src/test/java/mockito/MockitoExample.java
@@ -0,0 +1,22 @@
+package mockito;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class MockitoExample {
+
+	@Test
+	public void test() {
+		  List<String> mockList = mock(List.class);
+		    mockList.add("First");
+		    when(mockList.get(0)).thenReturn("Mockito");
+		    when(mockList.get(1)).thenReturn("JCG");
+		    assertEquals("Mockito", mockList.get(0));
+		    assertEquals("JCG", mockList.get(1));
+	}
+
+}
